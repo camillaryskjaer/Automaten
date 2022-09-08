af6972f4f192fb72b2fb4c104e0bfa0a169eb9ed
Danman05 <danielspurrell@hotmail.com>
2022-09-08 12:07:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Automaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
